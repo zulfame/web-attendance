9e92fc47e10edc359f0cd47e6349df158afe3a2f
Zulfadli Rizal <zulfadlirizal@gmail.com>
2023-02-24 16:49:46 +0700

    Membuat API Endpoin Attendance History

--- app/User.php
+++ app/User.php
@@ -38,7 +38,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function attendance()
+    public function attendances()
     {
         return $this->hasMany(Attendance::class);
     }
