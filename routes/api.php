f1bafd86ba57de37824b5579ae72e5741ed01ff0
Zulfadli Rizal <zulfadlirizal@gmail.com>
2023-02-24 15:42:40 +0700

    Membuat API Endpoin Register dll

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('/register', 'Api\Auth\AuthController@register');
+    Route::post('/login', 'Api\Auth\AuthController@login');
+    Route::post('/logout', 'Api\Auth\AuthController@logout')
+        ->middleware('auth:sanctum');
+
+    Route::post('/password/reset', 'Api\Auth\PasswordController@reset')
+        ->middleware('auth:sanctum');
+    Route::post('/password/forgot', 'Api\Auth\PasswordController@sendResetLinkEmail');
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::post('attendance', 'Api\AttendanceController@store');
+    Route::get('attendance/history', 'Api\AttendanceController@history');
+});
